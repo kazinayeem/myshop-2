86f3c721dfe231d61086486a17063f780830ce3a
kazinayeem <mohammadalinayeem360@gmail.com>
2025-03-01 13:48:07 +0600

    graph hide

--- admin-panel/src/pages/Dashboard.jsx
+++ admin-panel/src/pages/Dashboard.jsx
@@ -1,14 +1,14 @@
 import { Outlet } from "react-router";
 import SideBar from "../components/SideBar";
-import ProductList from "../components/ProductList";
-import OrderChart from "../components/OrderChart";
+// import ProductList from "../components/ProductList";
+// import OrderChart from "../components/OrderChart";
 export default function Dashboard() {
   return (
     <div className="flex">
       <SideBar />
       <div className="flex-1 p-6">
-        <ProductList/>
-        <OrderChart/>
+        {/* <ProductList/>
+        <OrderChart/> */}
         <Outlet />
       </div>
     </div>
