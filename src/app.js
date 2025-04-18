6c7c70a1b7a688314ef747c222976ee542426b8d
kazinayeem <mohammadalinayeem360@gmail.com>
2025-04-18 19:23:58 +0600

    chnage

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 import app from "./index.js";
 import connectDB from "./config/db.js";
 import dotenv from "dotenv";
+dotenv.config();
 import "./cronJobs/orderCleanup.js";
 import { envData } from "./config/envdata.js";
 
 const PORT = process.env.PORT || envData.PORT || 5000;
 
-dotenv.config();
+
 app.listen(PORT, () => {
   connectDB().then(() => {
     console.log(`Server is running on port ${PORT}`);
